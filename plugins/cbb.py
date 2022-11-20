a2a3350fc99fd1d8f31c610c341cb8c2746c7b31
CODE-BUDDY <111114081+BUDDY1609@users.noreply.github.com>
2022-11-20 22:39:16 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,11 +10,16 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>👨‍💻 Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n\n📚 Language : <code>Python3</code>\n\n📝 Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n\n📡 ʜᴏsᴛᴇᴅ ᴏɴ : Koyeb\n\n📢 ᴜᴘᴅᴀᴛᴇs ᴄʜᴀɴɴᴇʟ : <a href=https://t.me/tgnvs>ᴄʟɪᴄᴋ ʜᴇʀᴇ</a>\n\n🌟 ᴠᴇʀsɪᴏɴ : ᴠ 4.0\n</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
+                        InlineKeyboardButton('❤ Donation ', url='https://upier.vercel.app/pay/tgnvs@axisbank')
+                    ],[
+                        InlineKeyboardButton('〽️ 𝙐𝙥𝙙𝙖𝙩𝙚𝙨', url='https://t.me/tgnvs'),
+                        InlineKeyboardButton('🎬 𝙈𝙤𝙫𝙞𝙚 𝘾𝙝𝙖𝙣𝙣𝙚𝙡', url='https://t.me/nvsmovielink')
+                    ],[
                         InlineKeyboardButton("🔒 Close", callback_data = "close")
                     ]
                 ]
