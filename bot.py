1b1c7b0a765d1d3d606a0e935a5aef47cca183a7
CODE-BUDDY <111114081+BUDDY1609@users.noreply.github.com>
2023-01-03 15:17:07 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,10 @@
 
 from aiohttp import web
 from plugins import web_server
-
+from time import sleep, time
+from psutil import boot_time, disk_usage, net_io_counters
+from subprocess import check_output
+from os import path as ospath
 import pyromod.listen
 from pyrogram import Client
 from pyrogram.enums import ParseMode
@@ -67,6 +70,29 @@ class Bot(Client):
         await app.setup()
         bind_address = "0.0.0.0"
         await web.TCPSite(app, bind_address, PORT).start()
+        botStartTime = time()
+if ospath.exists('.git'):
+    commit_date = check_output(["git log -1 --date=format:'%y/%m/%d %H:%M' --pretty=format:'%cd'"], shell=True).decode()
+else:
+    commit_date = 'No UPSTREAM_REPO'
+
+@app.route('/status', methods=['GET'])
+def status():
+    bot_uptime = time() - botStartTime
+    uptime = time() - boot_time()
+    sent = net_io_counters().bytes_sent
+    recv = net_io_counters().bytes_recv
+    return {
+        'commit_date': commit_date,
+        'uptime': uptime,
+        'on_time': bot_uptime,
+        'free_disk': disk_usage('.').free,
+        'total_disk': disk_usage('.').total,
+        'network': {
+            'sent': sent,
+            'recv': recv,
+        },
+    }
 
     async def stop(self, *args):
         await super().stop()
